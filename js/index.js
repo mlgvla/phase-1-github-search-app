df8e75bbf687c67e979117ba3245f383cdd3ff90
mlgvla <mlgerardvla@gmail.com>
2022-02-27 19:53:21 -0500

    avatars

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
 })
 let form
 
+
 function setElements() {
   form = document.getElementById("github-form")
   form.addEventListener("submit", e => {
@@ -12,12 +13,34 @@ function setElements() {
 }
 
 function getUsers(user) {
-    fetch(`https://api.github.com/search/users?q=${user}`, {
-        method: "GET",
-        headers: {
-            Accept: "application/vnd.github.v3+json"
-        }
-    })
+  fetch(`https://api.github.com/search/users?q=${user}`, {
+    method: "GET",
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+    },
+  })
     .then(r => r.json())
-    .then(users => console.log(users))
+    .then(users => displayUsers(users.items))
+}
+
+function displayUsers(users) {
+
+    console.log(users)
+
+    users.forEach(user => {
+        buildUser(user)
+    });
+  
+}
+
+function buildUser(user) {
+    let ul = document.getElementById('user-list')
+    console.log(ul)
+    let li = document.createElement('li')
+    let avatarImg = document.createElement('img')
+    avatarImg.height = "50"
+    avatarImg.width = "50"
+    avatarImg.src = user.avatar_url
+    li.appendChild(avatarImg)
+    ul.appendChild(li)
 }
