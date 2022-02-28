d50862741d5e964098e3db935d3ba6c344b39338
mlgvla <mlgerardvla@gmail.com>
2022-02-27 23:25:40 -0500

    repos

--- js/index.js
+++ js/index.js
@@ -23,40 +23,52 @@ function getUsers(user) {
 }
 
 function displayUsers(users) {
+  let ul = document.getElementById("user-list")
+  ul.innerHTML = ""
+
   users.forEach(user => {
-    buildUser(user)
+    let li = document.createElement("li")
+
+    let avatarImg = document.createElement("img")
+    avatarImg.height = "50"
+    avatarImg.width = "50"
+    avatarImg.src = user.avatar_url
+
+    let userRepoLink = document.createElement("a")
+    userRepoLink.innerHTML = `&nbsp&nbsp&nbsp${user.login}&nbsp&nbsp&nbsp`
+    userRepoLink.addEventListener("click", () => getRepos(user.repos_url))
+
+    let profile = document.createElement("a")
+    profile.href = user.html_url
+    profile.innerHTML = "Profile" //user profile link
+
+    li.append(avatarImg, userRepoLink, profile)
+    ul.appendChild(li)
   })
 }
 
-function buildUser(user) {
-  let ul = document.getElementById("user-list")
-
-  let li = document.createElement("li")
-
-  let avatarImg = document.createElement("img")
-  avatarImg.height = "50"
-  avatarImg.width = "50"
-  avatarImg.src = user.avatar_url
-
-  let userRepoLink = document.createElement("a")
-  userRepoLink.innerHTML = `&nbsp&nbsp&nbsp${user.login}&nbsp&nbsp&nbsp`
-  userRepoLink.addEventListener("click", () => getRepos(user.repos_url))
-
-  let profile = document.createElement("a")
-  profile.href = user.html_url
-  profile.innerHTML = "Profile" //user profile link
-
-  li.append(avatarImg, userRepoLink, profile)
-  ul.appendChild(li)
-}
+function buildUser(user) {}
 
 function getRepos(url) {
   fetch(url, {
-      method: "GET",
-      headers: {
-        Accept: "application/vnd.github.v3+json",
-      }
+    method: "GET",
+    headers: {
+      Accept: "application/vnd.github.v3+json",
+    },
   })
-  .then(r => r.json())
-  .then(repos => console.log(repos))
+    .then(r => r.json())
+    .then(repos => displayRepos(repos))
+}
+
+function displayRepos(repos) {
+    let repoList = document.getElementById('repos-list')
+    repos.forEach((repo) => {
+        console.log(repo)
+      let li = document.createElement('li')
+      let repoLink = document.createElement('a')
+      repoLink.href = repo.html_url
+      repoLink.innerHTML = repo.full_name
+      li.appendChild(repoLink)
+      repoList.appendChild(li)
+    })
 }
